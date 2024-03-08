88f94fd448167829374af88385cfd792ecf5712d
Mohammad Jamshidi <mohamadjamshidiiii8@gmail.com>
2024-03-08 06:57:59 +0330

    feat: get inputs value and save in variables

--- Js/app.js
+++ Js/app.js
@@ -1,16 +1,56 @@
-let inputElem = document.querySelector('#input')
+let inputElem = document.querySelectorAll('#input')
+let calculateAge = document.querySelector('#submit')
+let userYear = null;
+let userMonth = null;
+let userDay = null;
 
 
-// console.log(input);
 
-// input.addEventListener('focus', (e) => {
-//     let divider = e.target.nextElementSibling;
+inputElem.forEach(input => {
+    input.addEventListener('focus', (e) => {
+        let divider = e.target.nextElementSibling
 
-//     divider.classList.remove('hidden')
-// })
+        divider.classList.remove('hidden')
+        divider.classList.add('bg-purple')
 
-// input.addEventListener('blur', (e) => {
-//     let divider = e.target.nextElementSibling;
 
-//     divider.classList.add('hidden')
-// })
+    })
+
+    input.addEventListener('blur', (e) => {
+        let divider = e.target.nextElementSibling
+
+        divider.classList.add('hidden')
+
+    })
+
+    input.addEventListener('input', (e) => {
+        // console.log(e.target.placeholder);
+
+        switch (e.target.placeholder) {
+            case 'DD': {
+                userDay = e.target.value
+                break;
+            }
+            case 'MM': {
+                userMonth = e.target.value
+                break;
+            }
+            case 'YY': {
+                userYear = e.target.value
+                break;
+            }
+        }
+    })
+})
+
+calculateAge.addEventListener('click', () => {
+    inputElem.forEach(input => {
+        input.value = ''
+
+        if (input.placeholder === 'DD') {
+
+            input.focus()
+            
+        }
+    })
+})
